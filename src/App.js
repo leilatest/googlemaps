b2bb8f4b5f4d45e27b10fd06f76915de9dfa13c3
leila ben chaalia <119710623+leilatest@users.noreply.github.com>
2023-03-26 02:57:03 +0100

    google maps

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Google Maps</h1>
+     <iframe width="1200" height="520" id="gmap_canvas" src="https://maps.google.com/maps?q=tunis%20beja%20gomycode&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}
+
+export default App;
